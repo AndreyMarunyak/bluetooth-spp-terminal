b13cf90898a42bb1f8ed0966c87eb1946c917e1b
AndreyMarunyak <andrey.marunyak@gmail.com>
2018-01-01 12:20:38 +0200

    New button and activity

--- bluetooth-spp-terminal/src/main/java/ru/sash0k/bluetooth_terminal/activity/MenuActivity.java
+++ bluetooth-spp-terminal/src/main/java/ru/sash0k/bluetooth_terminal/activity/MenuActivity.java
@@ -4,19 +4,43 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
 
 import ru.sash0k.bluetooth_terminal.R;
 
+
+
 public class MenuActivity extends Activity {
+
+    boolean check = false;
+
 
     public static void show(Context context) {
         Intent intent = new Intent(context, MenuActivity.class);
         context.startActivity(intent);
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+    }
+
+    public void LampCommand(View view) {
+        Button myBtn = (Button) findViewById(R.id.button);
+        if (check == true) {
+            check = false;
+            myBtn.setText("On");
+        }
+            else {
+            myBtn.setText("Off");
+            check = true;
+            }
     }
 }
